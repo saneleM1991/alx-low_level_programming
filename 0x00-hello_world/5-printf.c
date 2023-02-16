429d647880ce0ea2301dac0d8b5f89beb9cace22
saneleM1991 <91sanelem@gmail.com>
2023-02-16 02:20:30 -0800

    programm with printf fucntion

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - program that prints text using printf function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
