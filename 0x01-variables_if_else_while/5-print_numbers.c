4ca493881e51f1f1b898f395e94d558c54f560e5
saneleM1991 <91sanelem@gmail.com>
2023-02-17 02:31:41 -0800

    5 c code added

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entrypoint.
+ *
+ * Return:  0.(Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+
+	printf("\n");
+
+	return (0);
+}
