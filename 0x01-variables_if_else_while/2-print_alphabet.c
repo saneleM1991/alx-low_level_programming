d15151175132475b4dafed7f992402dcb8662d91
saneleM1991 <91sanelem@gmail.com>
2023-02-17 01:37:33 -0800

    print alphabet c code added

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the application
+ * Return: 0(Success)
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter != 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+
+	return (0);
+}
