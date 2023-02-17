e89fca4deb6cfc41e48339fdb3dd87983689b469
saneleM1991 <91sanelem@gmail.com>
2023-02-17 02:14:07 -0800

    4 c code added

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - EntryPoint of the programm
+ * Return: 0(Success)
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter != 'z')
+	{
+		if (letter != 'e' && letter != 'q')
+			putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+
+	return (0);
+}
