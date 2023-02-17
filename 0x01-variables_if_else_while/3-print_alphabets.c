673cbc0377cbc99e2bd45f36160e91baab8561f3
saneleM1991 <91sanelem@gmail.com>
2023-02-17 01:54:18 -0800

    3 print alphabets c code added

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entrypoint for the application
+ * Return: 0(Success)
+ */
+int main(void)
+{
+	char small_letter = 'a';
+	char capital_letter = 'A';
+
+	while (small_letter != 'z')
+	{
+		putchar(small_letter);
+		small_letter++;
+	}
+	while (capital_letter != 'Z')
+	{
+		putchar(capital_letter);
+		capital_letter++;
+	}
+	putchar('\n');
+
+	return (0);
+}
