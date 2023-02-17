be8cc9e0b28ec1023ed725192967b9fd80c7534f
saneleM1991 <91sanelem@gmail.com>
2023-02-16 20:05:03 -0800

    101 fixed script

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
+
 /**
- * main - Entry point
- * Return: Always 1 (Success)
- */
+*main - Entry point
+*Return: Always 1
+*/
+
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful"
-			"\" - Dora Korpar, 2015-10-19\n");
-
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
