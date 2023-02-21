b272d82fa1d7034e934c9d9f4738ca5aa0fa2f63
saneleM1991 <91sanelem@gmail.com>
2023-02-21 07:11:46 -0800

    add function added

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - Adds twoo number and return result
+ * @num_one: first number
+ * @num_twoo: second number
+ * Return: sum of num_one and num_twoo
+ */
+int add(int num_one, int num_twoo)
+{
+	return (num_one + num_twoo);
+}
