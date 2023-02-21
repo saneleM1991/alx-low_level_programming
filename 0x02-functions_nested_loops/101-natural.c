f457e9aaecec661313af1b3a156bb6d53935a8c5
saneleM1991 <91sanelem@gmail.com>
2023-02-21 08:06:43 -0800

    natural number function added

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the application
+ * Return: 0 Succes or 1 for Error
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = 1; a < 1024; a++)
+	{
+		if ((a % 3) == 0 || (a % 5) == 0)
+		{
+			b += a;
+		}
+	}
+	printf("%d\n", b);
+	return (0);
