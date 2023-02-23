65897c14dcbbb9e9af249a91436bda9bc417ff9d
saneleM1991 <91sanelem@gmail.com>
2023-02-23 00:12:55 -0800

    2 largets bunerb fixe added

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * largest_number - returns the largest of 3 numbers
  * @a: first integer
@@ -6,24 +7,22 @@
  * @c: third integer
  * Return: largest number
  */
+
 int largest_number(int a, int b, int c)
+{int largest;
+
+if (a > b && a > c)
 {
- int largest;
- if (a > b && a > c)
- {
- largest = a;
- }
- else if (a > b && c > a)
- {
- largest = c;
- }
- else if (b > c)
- {
- largest = b;
- }
- else
- {
- largest = c;
- }
+largest = a;
+}
+else if (b > c)
+{
+largest = b;
+}
+else
+{
+largest = c;
+}
+
 return (largest);
 }
