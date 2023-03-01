043e07f6e35e65c9a5e61ee1f2bad10adb30bb55
saneleM1991 <91sanelem@gmail.com>
2023-03-01 10:40:25 -0800

    print number function added

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,5 +10,7 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *z);
+char *rot13(char *s);
+void print_number(int n);
 
 #endif /* MAIN_H */
