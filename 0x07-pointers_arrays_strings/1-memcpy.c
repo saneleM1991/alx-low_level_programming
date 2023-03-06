95b41f313e643590836b5671a662dbd223cb190a
saneleM1991 <91sanelem@gmail.com>
2023-03-06 03:05:44 -0800

    memcpy fixed

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,12 +8,10 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	for (int i = 0; i < n; i++)
-	{
-		*dest = *src;
-		src++;
-		dest++;
-	}
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
 
 	return (dest);
 }
