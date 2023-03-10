7f6cfec92862ec1dd4e8b3114de19e5281afb1f2
saneleM1991 <91sanelem@gmail.com>
2023-03-10 07:02:37 -0800

    0-whatsmyname fix added

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
+#include "main.h"
 /**
  * main - Entry point
  * @argc: numbers arguments passed
  * @argv: pointter to 1 daimentional array
  * Return: 0 success or 1 error
 */
-
 int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
