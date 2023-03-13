dd36d0581ac702b751a140f4d464ebe83667b6b8
saneleM1991 <91sanelem@gmail.com>
2023-03-13 08:55:58 -0700

    create-array function fift fixer added

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,12 +11,13 @@
 char *create_array(unsigned int size, char c)
 {
 	char *p;
+	unsigned int i;
 
 	if (size <= 0)
 		return (NULL);
 
 	p = (char *)malloc(size * sizeof(char));
-	unsigned int i = 0;
+	i = 0;
 
 	if (p != NULL)
 	{
