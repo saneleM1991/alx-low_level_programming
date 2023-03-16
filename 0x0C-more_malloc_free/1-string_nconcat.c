5aaa627b73e424680b64d7b24874c8b4cfc15eee
saneleM1991 <91sanelem@gmail.com>
2023-03-16 14:01:37 -0700

    string n cat fixed

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,35 +10,32 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *new_str;
-	unsigned int s1_len, s2_len;
-	unsigned int i, j;
+	unsigned int i, j, k;
+	char *s;
 
 	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-
-	s1_len = strlen(s1);
-	s2_len = strlen(s2);
-	new_str = (char *)malloc((s1_len + s2_len + 1) * sizeof(char));
-
-	if (new_str == NULL)
-		return (NULL);
-
-	for (i = 0; i < s1_len; i++)
-		new_str[i] = s1[i];
-
-	if (n >= s2_len)
-	{
-		for (j = 0; i < (s1_len + s2_len + 1); i++, j++)
-			new_str[i] = s2[j];
-	}
+		i = 0;
 	else
 	{
-		for (j = 0; j <= n; i++, j++)
-			new_str[i] = s2[j];
-		new_str[i] = '\0';
+		for (i = 0; s1[i]; i++)
+			;
 	}
-	return (new_str);
+	if (s2 == NULL)
+		j = 0;
+	else
+	{
+		for (j = 0; s2[j]; j++)
+			;
+	}
+	if (j > n)
+		j = n;
+	s = malloc(sizeof(char) * (i + j + 1));
+	if (s == NULL)
+		return (NULL);
+	for (k = 0; k < i; k++)
+		s[k] = s1[k];
+	for (k = 0; k < j; k++)
+		s[k + i] = s2[k];
+	s[i + j] = '\0';
+	return (s);
 }
