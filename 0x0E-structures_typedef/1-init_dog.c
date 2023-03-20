d58f150356370853cc3b1866cb7fb40357b31f08
saneleM1991 <91sanelem@gmail.com>
2023-03-20 07:28:15 -0700

    init dog added

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+/**
+ * init_dog - initialise dog
+ * @d: a dog to initialise
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
+
