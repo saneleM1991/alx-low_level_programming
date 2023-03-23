79bea9343e03da5f7b57ae6c7cfc0e537c06b526
saneleM1991 <91sanelem@gmail.com>
2023-03-23 09:20:16 -0700

    print all added

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,9 +1,22 @@
 #ifndef VARIADIC_F
 #define VARIADIC_F
+#include <stdarg.h>
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+
+/**
+ * struct format - Data type of a format.
+ * @op: Format.
+ * @f: Function.
+ *
+ */
+typedef struct format
+{
+	char *op;
+	void (*f)(va_list all);
+} f;
 
 #endif
