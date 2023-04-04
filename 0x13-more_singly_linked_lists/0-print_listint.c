4260e240346206f4f918f31e788dfb7b5614b80d
saneleM1991 <91sanelem@gmail.com>
2023-04-03 22:42:50 -0700

    pritn list bog fix 2.1

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,11 +10,11 @@ size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
 
-	while (h != NULL)
+	while (h)
 	{
-		printf("%lu\n", h->n);
-		h = h->next;
 		count++;
+		printf("%d\n", h->n);
+		h = h->next;
 	}
 
 	return (count);
