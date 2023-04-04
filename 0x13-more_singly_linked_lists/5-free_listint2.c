6b898d0148f52a154b304387004c970516ab8f02
saneleM1991 <91sanelem@gmail.com>
2023-04-04 02:32:56 -0700

    free list 2 added

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint2 - free linked list
+ * @head: head node pointer
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp_ptr1 = *head;
+	listint_t *temp_ptr2 = *head;
+
+	while (temp_ptr1 != NULL)
+	{
+		temp_ptr2 = temp_ptr1->next;
+		free(temp_ptr1);
+		temp_ptr1 = temp_ptr2;
+	}
+	*head = temp_ptr1;
+}
