7012b5f1e86ef37b30767c5b49ca1c05d38abba0
saneleM1991 <91sanelem@gmail.com>
2023-04-04 07:58:16 -0700

    6-pop_listint added

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * pop_listint - remove head node and return data
+ * @head: head node pointer
+ * Return: node data | 0 if list is emtpy
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t temp_ptr;
+
+	if (*head == NULL)
+		return (0);
+
+	temp_ptr = *head->next;
+
+	free(*head);
+	*head = temp_prt;
+}
