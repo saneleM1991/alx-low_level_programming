51f9576a149e670d15ae96a3ee2939bc755a59a5
saneleM1991 <91sanelem@gmail.com>
2023-04-04 09:00:48 -0700

    7-get node at index fixer1 added

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -21,3 +21,20 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	return (head);
 }
+/**
+ * listint_len - prints the lenght of the list
+ * @h: head node pointer
+ * Return: number of node | 0 if its emtpy
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t n;
+
+	n = 0;
+	while (h != NULL)
+	{
+		n++;
+		h = h->next;
+	}
+	return (n);
+}
