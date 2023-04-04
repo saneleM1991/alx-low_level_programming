5bc4a8fe6b76d1f3cfcb88b6299c3157a23e7f9b
saneleM1991 <91sanelem@gmail.com>
2023-04-04 07:45:07 -0700

    free list 2 fixed

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,17 +7,15 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *temp_ptr1 = *head;
-	listint_t *temp_ptr2 = *head;
+	listint_t *temp_ptr = *head;
 
 	if (*head == NULL)
 		return;
 
-	while (temp_ptr1 != NULL)
+	while (temp_ptr != NULL)
 	{
-		temp_ptr2 = temp_ptr1->next;
-		free(temp_ptr1);
-		temp_ptr1 = temp_ptr2;
+		*head = temp_ptr->next;
+		free(temp_ptr);
+		temp_ptr = *head;
 	}
-	*head = temp_ptr1;
 }
