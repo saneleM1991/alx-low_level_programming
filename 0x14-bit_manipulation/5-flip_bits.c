3c59876118dfa87ad832177decaabdd449c9bd23
saneleM1991 <91sanelem@gmail.com>
2023-04-10 11:10:20 -0700

    flip bits added

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * flip_bits - get number flips o get from one number to another
+ * @n: number to check to
+ * @m: number of flips
+ * Return: number of bits you would need to flip
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	return ((~n)|(~m) << 1);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -7,7 +7,7 @@ void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
-
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
 
 int is_binary(const char *str);
 double _pow(double base, int exponent);
