5d5bd19996f66a7643ef4b46ed19ecd79c3607c3
saneleM1991 <91sanelem@gmail.com>
2023-04-10 07:15:36 -0700

    binary to uni added

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+
+
+int is_binary(const char *str);
+double _pow(double base, int exponent);
+
+
+#endif
