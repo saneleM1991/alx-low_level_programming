62d94039cd9c230af7ec9a18b14b752e22daa7b8
saneleM1991 <91sanelem@gmail.com>
2023-07-06 23:43:40 +0200

    flib bits bug fix

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -8,5 +8,16 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	return ((~n) | (~m) << 1);
+	int i, count = 0;
+	unsigned long int current;
+	unsigned long int exclusive = n ^ m;
+
+	for (i = 63; i >= 0; i--)
+	{
+		current = exclusive >> i;
+		if (current & 1)
+			count++;
+	}
+
+	return (count);
 }
